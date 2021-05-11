96a69d0cbd3ebb9f2ca10b235f63b7ad270a4b65
ashley <nguyenthanhcong101096@gmail.com>
2021-05-11 16:04:40 +0700

    tmp

--- manifest/create_resources.sh
+++ manifest/create_resources.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# kubectl create -f database_volume.yml
 kubectl create -f secret.yml
 kubectl create -f configmap.yml
-kubectl create -f database_volume.yml
+kubectl create -f database_daemonset.yml
 kubectl create -f deployment/rails_deployment.yml
-kubectl create -f deployment/psql_deployment.yml
+# kubectl create -f deployment/mysql_deployment.yml
 kubectl create -f services/rails_svc.yml
-kubectl create -f services/psql_svc.yml
+kubectl create -f services/mysql_svc.yml
