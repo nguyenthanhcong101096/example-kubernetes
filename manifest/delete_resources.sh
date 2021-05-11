2f87f1d20d684170adf894d4ce1efffa0e2a7831
ashley <nguyenthanhcong101096@gmail.com>
2021-05-11 17:06:46 +0700

    tmp

--- manifest/delete_resources.sh
+++ manifest/delete_resources.sh
@@ -3,6 +3,7 @@
 kubectl delete svc --all
 kubectl delete deploy --all
 kubectl delete ds --all
+kubectl delete job --all
 kubectl delete secret --all
 kubectl delete configMap --all
 kubectl delete pvc --all
