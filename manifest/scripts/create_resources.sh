91a69ca0276132cb15b0e4e619c867cf7e20e334
congnt <nguyenthanhcong101096@gmail.com>
2021-05-11 21:36:55 +0700

    tmp

--- manifest/scripts/create_resources.sh
+++ manifest/scripts/create_resources.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # kubectl create -f database_volume.yml
-kubectl create -f secret.yml
-kubectl create -f configmap.yml
-kubectl create -f daemon-sets/database_ds.yml
-kubectl create -f daemon-sets/redis_ds.yml
-kubectl create -f deployment/rails_deployment.yml
-kubectl create -f services/rails_svc.yml
-kubectl create -f services/mysql_svc.yml
-kubectl create -f services/redis_svc.yml
+kubectl create -f ../secret.yml
+kubectl create -f ../configmap.yml
+kubectl create -f ../daemon-sets/database_ds.yml
+kubectl create -f ../daemon-sets/redis_ds.yml
+kubectl create -f ../deployment/rails_deployment.yml
+kubectl create -f ../services/rails_svc.yml
+kubectl create -f ../services/mysql_svc.yml
+kubectl create -f ../services/redis_svc.yml
--- manifest/scripts/delete_resources.sh
+++ manifest/scripts/delete_resources.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-kubectl delete svc --all
-kubectl delete deploy --all
-kubectl delete ds --all
-kubectl delete job --all
-kubectl delete secret --all
-kubectl delete configMap --all
-kubectl delete pvc --all
-kubectl delete pv --all
+kubectl delete svc --all -n k8s
+kubectl delete deploy --all -n k8s
+kubectl delete ds --all -n k8s
+kubectl delete job --all -n k8s
+kubectl delete secret --all -n k8s
+kubectl delete configMap --all -n k8s
+kubectl delete pvc --all -n k8s
+kubectl delete pv --all -n k8s
