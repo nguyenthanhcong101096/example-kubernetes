e238b31d3186e1587f071213db21e2855a462ab6
nguyenthanhcong101096 <nguyenthanhcong101096@gmail.com>
2021-05-11 21:59:05 +0700

    tmp

--- manifest/scripts/create_resources.sh
+++ manifest/scripts/create_resources.sh
@@ -5,7 +5,7 @@ kubectl create -f ../secret.yml
 kubectl create -f ../configmap.yml
 kubectl create -f ../daemon-sets/database_ds.yml
 kubectl create -f ../daemon-sets/redis_ds.yml
-kubectl create -f ../deployment/rails_deployment.yml
+kubectl create -f ../deployments/rails_deployment.yml
 kubectl create -f ../services/rails_svc.yml
 kubectl create -f ../services/mysql_svc.yml
 kubectl create -f ../services/redis_svc.yml
